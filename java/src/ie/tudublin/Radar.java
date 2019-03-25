cecef34cde8df919666e46c7f5a40dcc87b82645
Apple <Apple@Apples-MacBook-Pro.local>
2019-03-25 18:49:08 +0000

    create a rectangle

--- java/src/ie/tudublin/Radar.java
+++ java/src/ie/tudublin/Radar.java
@@ -29,4 +29,25 @@ public class Radar
         this.j = j; //random(420,140)
         
     }
+
+    public void rect()
+    {
+        ui.fill(0,0,255);
+        ui.stroke(0,0,255);
+        ui.rect(10,400,200,180);
+
+        float howFast = 2;
+        float size1 = 10;
+        int num =0;
+        ui.fill(255,218,185);
+      
+        // while(num <= 5)
+        // {
+        //     ui.ellipse(i[num], j[num], size1, size1);
+        //     num++;
+        // }
+        
+        ui.ellipse(i, j, size1, size1);
+		//j = j + 0.1f;
+    }
 }
